ee70214e8a2aec1db9d79a830055e059a7ae3b8b
Konstantin <rewertkin@gmail.com>
2024-11-18 21:42:24 +0300

    полное решение

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -28,17 +28,44 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 9;
+        double b = 5;
+        double c = 30;
 
         System.out.println(solveEquation(a, b, c));
 
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 & b == 0 & c != 0) {
+            return "Некорректные входные данные";
+        } else if (a == 0 & b == 0 & c == 0) {
+            return "Бесконечное множество решений";
+        }
+
+        if (a != 0) {
+            double d = Math.pow(b, 2) - (4 * a * c);
+            if (d == 0) {
+                return String.format("Количество решений: 1. Корень: %.0f", -b / 2 * a);
+            } else if (d > 0) {
+
+                double x1 = (-b - Math.sqrt(d)) / 2 * a;
+                double x2 = (-b + Math.sqrt(d)) / 2 * a;
+                if (x2 > x1) {
+                    double i = x1;
+                    x1 = x2;
+                    x2 = i;
+                }
+                return String.format("Количество решений: 2. Корни: %.0f;%.0f", x1, x2);
+            } else {
+                return "Количество решений: 0.";
+            }
+        } else {
+            if (c == 0) {
+                return "Количество решений: 1. Корень: 0";
+            }
+            return String.format("Количество решений: 1. Корень: %.0f", -c / b);
+        }
     }
 }
