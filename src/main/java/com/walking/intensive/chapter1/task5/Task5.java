5da1a3d39e16d38abaabdb480fed91c970af12d8
Konstantin <rewertkin@gmail.com>
2024-11-19 17:41:46 +0300

    решение таска 5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Задача поиска площади, величин углов, длин высот, биссектрис, медиан, радиусов вписанной и описанной вокруг
  * треугольника окружностей является центральной в Геометрии.
@@ -11,6 +13,9 @@ package com.walking.intensive.chapter1.task5;
 public class Task5 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        double x = getAreaAdvanced(12, 13, 5);
+        System.out.format("%.2f", x);
+        //System.out.print(Math.asin(0.3846));
     }
 
     /**
@@ -24,8 +29,11 @@ public class Task5 {
      */
     static double getAreaByHeron(double a, double b, double c) {
         //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if(checkFailSides(a, b, c)){
+            return -1;
+        }else{
+            return  (double) 1 / 4 * Math.sqrt((a + b - c) * (a - b + c) * (-a + b + c) * (a + b + c));
+        }
     }
 
     /**
@@ -36,9 +44,24 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double[] arrayOfHeights = new double[0];
+
+        if (checkFailSides(a,b,c)){
+            return arrayOfHeights;
+        }
+
+        double s = getAreaByHeron(a, b, c);
+
+        arrayOfHeights = new double[3];
+        double[] arraySides = {a, b, c};
+
+        for (int i = 0; i < 3; i++){
+            arrayOfHeights[i] = 2 * s / arraySides[i];
+        }
+
+        Arrays.sort(arrayOfHeights);
+        return arrayOfHeights;
     }
 
     /**
@@ -49,9 +72,25 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double[] arrayOfMedians = new double[0];
+
+        if (checkFailSides(a, b, c)){
+            return arrayOfMedians;
+        }
+        arrayOfMedians = new double[3];
+
+        for (int i = 0;i < 3; ++i){
+            arrayOfMedians[i] = switch (i) {
+                case 0 -> Math.sqrt(2 * Math.pow(b, 2) + 2 * Math.pow(c, 2) - Math.pow(a, 2)) * 1 / 2;
+                case 1 -> Math.sqrt(2 * Math.pow(a, 2) + 2 * Math.pow(c, 2) - Math.pow(b, 2)) * 1 / 2;
+                case 2 -> Math.sqrt(2 * Math.pow(a, 2) + 2 * Math.pow(b, 2) - Math.pow(c, 2)) * 1 / 2;
+                default -> 0;
+            };
+        }
+        Arrays.sort(arrayOfMedians);
+
+        return arrayOfMedians;
     }
 
     /**
@@ -62,9 +101,25 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] arrayOfBisectors = new double[0];
 
-        return null; // Заглушка. При реализации - удалить
+        if (checkFailSides(a, b, c)){
+            return arrayOfBisectors;
+        }
+
+        arrayOfBisectors = new double[3];
+
+        for (int i = 0;i < 3; ++i){
+            arrayOfBisectors[i] = switch (i) {
+                case 0 -> (Math.sqrt(a* b * (a + b + c) * (a + b - c))) / (a + b);
+                case 1 -> (Math.sqrt(a* c * (a + b + c) * (a + c - b))) / (a + c);
+                case 2 -> (Math.sqrt(c* b * (a + b + c) * (c + b - a))) / (c + b);
+                default -> 0;
+            };
+        }
+
+        Arrays.sort(arrayOfBisectors);
+        return arrayOfBisectors;
     }
 
     /**
@@ -76,8 +131,28 @@ public class Task5 {
      */
     static double[] getAngles(double a, double b, double c) {
         //        Место для вашего кода
+        double[] arrayOfAngles= new double[0];
 
-        return null; // Заглушка. При реализации - удалить
+        if (checkFailSides(a, b, c)){
+            return arrayOfAngles;
+        }
+
+
+        arrayOfAngles = new double[3];
+
+        double area = getAreaByHeron(a, b, c);
+
+        for (int i = 0;i < 3; ++i){
+            arrayOfAngles[i] = switch (i) {
+                case 0 -> Math.toDegrees(Math.asin((2 * area) / (a * b)));
+                case 1 -> Math.toDegrees(Math.asin((2 * area) / (b * c)));
+                case 2 -> Math.toDegrees(Math.asin((2 * area) / (a * c)));
+                default -> 0;
+            };
+        }
+
+        Arrays.sort(arrayOfAngles);
+        return arrayOfAngles;
     }
 
     /**
@@ -89,8 +164,10 @@ public class Task5 {
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
         //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if(checkFailSides(a, b, c)) {
+            return -1;
+        }
+        return getAreaByHeron(a, b, c) / ((a+ b + c) / 2);
     }
 
     /**
@@ -101,9 +178,10 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if(checkFailSides(a, b, c)) {
+            return -1;
+        }
+        return (a * b * c) / (4 * getAreaByHeron(a, b, c));
     }
 
     /**
@@ -121,8 +199,21 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+        if(checkFailSides(a, b, c)) {
+            return -1;
+        }
+        //найдем косинус
+        double cos = (a * a + b * b - c * c) / (2 * a * b);
+
+        //найдем синус
+        double sin = Math.sqrt(1 - cos * cos );
+
+        //найдем площадь
+        return a * b * sin / 2;
+    }
+
+    static boolean checkFailSides(double a, double b, double c) {
+        return !(b + c > a & a + c > b & a + b > c & a > 0 & b > 0 & c > 0);
     }
 }
