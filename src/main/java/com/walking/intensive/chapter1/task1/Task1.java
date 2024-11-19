e9ab8633c39d67d25dfaf25f2562bd7223c85200
Konstantin <rewertkin@gmail.com>
2024-11-19 14:13:24 +0300

    изменил return(убрал else)

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -13,7 +13,7 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         int age = 12;
 
         System.out.println(getAgeString(age));
