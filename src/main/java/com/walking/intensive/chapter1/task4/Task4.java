3f35e8c0a784ce416b76faac348ed0076ad8e633
Konstantin <rewertkin@gmail.com>
2024-11-20 14:05:42 +0300

    поменял &  на && - т.к. если слева false, дальше проверять не надо
    сделал сверху отдельное условие  a == 0, что бы в дальнейшем убрать условие a != 0
    
    Перенес "Бесконечное множество решений" наверх, убрал лишнее условие в "Некорректные входные данные"

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -40,42 +40,39 @@ public class Task4 {
 
     static String solveEquation(double a, double b, double c) {
 
-        if (a == 0 & b == 0 & c != 0) {
-            return "Некорректные входные данные";
-        }
-
-        if (a == 0 & b == 0 & c == 0) {
+        if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений";
         }
 
-        double discriminant = 0;
-        if (a != 0){
-            discriminant = Math.pow(b, 2) - (4 * a * c);
+        if (a == 0 && b == 0) {
+            return "Некорректные входные данные";
         }
 
-        if (a != 0 & discriminant == 0) {
+        if (a == 0 && (c == 0 || b == 0)) {
+            return "Количество решений: 1. Корень: 0";
+        }
+
+        if (a == 0) {
+            return String.format("Количество решений: 1. Корень: %.0f", -c / b);
+        }
+
+        double discriminant = Math.pow(b, 2) - (4 * a * c);
+
+        if (discriminant == 0) {
             return String.format("Количество решений: 1. Корень: %.0f", -b / 2 * a);
         }
-        if (a != 0 & discriminant > 0) {
+
+        if (discriminant > 0) {
             double x1 = (-b - Math.sqrt(discriminant)) / 2 * a;
             double x2 = (-b + Math.sqrt(discriminant)) / 2 * a;
-            if (x2 > x1){
+            if (x2 > x1) {
                 double temp = x1;
                 x1 = x2;
                 x2 = temp;
             }
-
             return String.format("Количество решений: 2. Корни: %.0f;%.0f", x1, x2);
         }
 
-        if (a != 0 & discriminant < 0) {
-            return "Количество решений: 0.";
-        }
-
-        if (a == 0 & ( c == 0 || b == 0)) {
-            return "Количество решений: 1. Корень: 0";
-        }
-
-            return String.format("Количество решений: 1. Корень: %.0f", -c / b);
+        return "Количество решений: 0.";
     }
 }
